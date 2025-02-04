dc231e6c36fd6896d0efbb404712fe16e3f8972f
Anish Manissery <anishmanissery@gmail.com>
2025-02-04 15:59:36 +0530

    Refactor navbar implementation: move HTML directly into index files and remove external navbar file

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// Include Top Navbar
-fetch('/includes/navbar.html?v=1.1')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('navbarContainer').innerHTML = data;
-
-    // Highlight the current page
-    const path = window.location.pathname;
-    const links = {
-        '/howtouse/': 'howtouse-link',
-        '/calculation/': 'calculation-link',
-        '/analysis/': 'analysis-link',
-        '/sponsor/': 'sponsor-link'
-    };
-
-    if (links[path]) {
-        document.getElementById(links[path]).classList.add('active');
-    }
-  })
-  .catch(error => console.error('Error loading navbar:', error));
-
-
 // Include Sponsors List
 fetch('/includes/sponsorslist.html?v=1.1')
   .then(response => response.text())
